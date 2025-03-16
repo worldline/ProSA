15974f798ca26545f333c2dbefb2ba61f8a9eb83
Jeremy HERGAULT <jeremy@hergault.fr>
2025-03-16 15:20:26 +0100

    feat: Implement queues
    Signed-off-by: Jeremy HERGAULT <jeremy@hergault.fr>

---
+++ prosa_utils/src/queue.rs
@@ -0,0 +1,92 @@
+//! Module for ProSA internal queueing utilitary
+
+/// Atomic queue implementation
+pub(crate) mod lockfree;
+
+/// Queue for single producer and multiple consumers
+pub mod spmc;
+
+/// Queue for Multiple producers and single consumer
+pub mod mpsc;
+
+/// Error define for Queues
+/// Use by utilitary queues an their implementation in ProSA.
+#[derive(Debug, Eq, thiserror::Error, PartialOrd, PartialEq)]
+pub enum QueueError<T> {
+    /// Error indicating that the queue is empty
+    #[error("The queue is empty")]
+    Empty,
+    /// Error indicating that the queue is full
+    #[error("The queue is full, it contain {1} items")]
+    Full(T, usize),
+    /// Can't retrieve the element
+    #[error("Can't retrieve the element {0}")]
+    Retrieve(usize),
+}
+
+/// Trait to define all information getter from the queue
+pub trait QueueChecker<P> {
+    /// Checks if the queue is empty.
+    /// Prefer this method over `len() != 0`
+    fn is_empty(&self) -> bool;
+    /// Checks if the queue is full.
+    /// Prefer this method over `len() != max_capacity()`
+    fn is_full(&self) -> bool;
+    /// Returns the number of item in the queue.
+    fn len(&self) -> P;
+    /// Returns the maximum item capacity of the queue.
+    fn max_capacity(&self) -> P;
+}
+
+/// Macro to define queue inner method related to `QueueChecker` trait
+macro_rules! impl_queue_checker {
+    ( $p:ty ) => {
+        fn is_empty(&self) -> bool {
+            self.get_head() == self.get_tail()
+        }
+
+        fn is_full(&self) -> bool {
+            (self.get_tail() + 1) % (N as $p) == self.get_head()
+        }
+
+        fn len(&self) -> $p {
+            let head = self.get_head();
+            let tail = self.get_tail();
+
+            if tail >= head {
+                tail - head
+            } else {
+                (self.max_capacity() - head) + tail
+            }
+        }
+
+        fn max_capacity(&self) -> $p {
+            N as $p
+        }
+    };
+}
+pub(crate) use impl_queue_checker;
+
+#[macro_export]
+/// Macro of an expression to know if an id is still contain by the queue that have a circular buffer
+///
+/// The buffer have two pointer that indicate head and tail.
+/// Every ID in this range is considered for the queue.
+///
+/// In the following example:
+/// - `h` represent head position
+/// - `t` represent tail position
+/// - `o` active items
+/// - `x` represent inactive items
+///
+/// If head is before the tail:
+/// `x h[ o [t x`
+///
+/// If head is after the tail:
+/// `o [t x h[ o`
+macro_rules! id_in_queue {
+    ( $id:ident, $head:ident, $tail:ident ) => {
+        ($head > $tail && ($id >= $head || $id < $tail)) || ($id >= $head && $id < $tail)
+    };
+}
+pub use id_in_queue;
---
+++ prosa_utils/src/queue/spmc.rs
@@ -0,0 +1,60 @@
+use super::{QueueChecker, QueueError};
+
+crate::queue::lockfree::impl_lockfree_queue!(
+    LockFreeOptQueueU16,
+    u16,
+    std::sync::atomic::AtomicU16,
+    Option<T>,
+    false,
+    true,
+    true
+);
+
+crate::queue::lockfree::impl_lockfree_queue!(
+    LockFreeOptQueueU32,
+    u32,
+    std::sync::atomic::AtomicU32,
+    Option<T>,
+    false,
+    true,
+    true
+);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, Clone, PartialEq)]
+    struct Data {
+        val: String,
+    }
+
+    impl Data {
+        fn new(val: String) -> Data {
+            Data { val }
+        }
+    }
+
+    macro_rules! queue_atomic_test {
+        ( $queue:ident ) => {
+            let queue = $queue::<Data, 4096>::default();
+            assert!(queue.is_empty());
+            assert_eq!(0, queue.len());
+            unsafe { assert_eq!(Ok((0, 0)), queue.push(Data::new("test".into()))) };
+            assert_eq!(1, queue.len());
+            assert_eq!(Ok(Data::new("test".into())), queue.pull());
+            assert!(queue.is_empty());
+            assert_eq!(0, queue.len());
+        };
+    }
+
+    #[tokio::test]
+    async fn queue_atomic_u16_test() {
+        queue_atomic_test!(LockFreeOptQueueU16);
+    }
+
+    #[tokio::test]
+    async fn queue_atomic_u32_test() {
+        queue_atomic_test!(LockFreeOptQueueU32);
+    }
+}
