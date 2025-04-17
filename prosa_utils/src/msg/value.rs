480588487ee1d7f39fe577ca2dab918f6c01ea20
Olivier SCHYNS <olivier.schyns@worldline.com>
2025-04-17 22:15:49 +0200

    feat: Implementing serde serialisation for TVF buffer

--- prosa_utils/src/msg/value.rs
+++ prosa_utils/src/msg/value.rs
@@ -3,11 +3,14 @@
 use super::tvf::Tvf;
 use bytes::Bytes;
 use chrono::{NaiveDate, NaiveDateTime};
-use std::borrow::Cow;
+use std::{borrow::Cow, fmt::Debug};
 
 /// A TVF value stored in the field of a TVF buffer
 #[derive(Debug, Clone)]
-pub enum TvfValue<'m, M: Tvf + Clone> {
+pub enum TvfValue<'v, T>
+where
+    T: Tvf + Clone,
+{
     /// A byte value
     Byte(u8),
 
@@ -21,10 +24,10 @@ pub enum TvfValue<'m, M: Tvf + Clone> {
     Float(f64),
 
     /// A string value
-    String(Cow<'m, String>),
+    String(Cow<'v, String>),
 
     /// A buffer of bytes value
-    Bytes(Cow<'m, Bytes>),
+    Bytes(Cow<'v, Bytes>),
 
     /// A date value
     Date(NaiveDate),
@@ -33,12 +36,12 @@ pub enum TvfValue<'m, M: Tvf + Clone> {
     DateTime(NaiveDateTime),
 
     /// A TVF sub buffer
-    Buffer(Cow<'m, M>),
+    Buffer(Cow<'v, T>),
 }
 
 macro_rules! impl_from {
     ($num_type:ty => $enum_type:ident as $cast_type:ty) => {
-        impl<'m, M: Tvf + Clone> From<$num_type> for TvfValue<'m, M> {
+        impl<'v, T: Tvf + Clone> From<$num_type> for TvfValue<'v, T> {
             fn from(value: $num_type) -> Self {
                 Self::$enum_type(value as $cast_type)
             }
@@ -59,38 +62,38 @@ impl_from!(isize => Signed as i64);
 impl_from!(f32 => Float as f64);
 impl_from!(f64 => Float as f64);
 
-impl<M: Tvf + Clone> From<&str> for TvfValue<'_, M> {
+impl<T: Tvf + Clone> From<&str> for TvfValue<'_, T> {
     fn from(value: &str) -> Self {
         Self::String(Cow::Owned(value.to_string()))
     }
 }
 
-impl<M: Tvf + Clone> From<String> for TvfValue<'_, M> {
+impl<T: Tvf + Clone> From<String> for TvfValue<'_, T> {
     fn from(value: String) -> Self {
         Self::String(Cow::Owned(value))
     }
 }
 
-impl<M: Tvf + Clone> From<Bytes> for TvfValue<'_, M> {
+impl<T: Tvf + Clone> From<Bytes> for TvfValue<'_, T> {
     fn from(value: Bytes) -> Self {
         Self::Bytes(Cow::Owned(value))
     }
 }
 
-impl<M: Tvf + Clone> From<NaiveDate> for TvfValue<'_, M> {
+impl<T: Tvf + Clone> From<NaiveDate> for TvfValue<'_, T> {
     fn from(value: NaiveDate) -> Self {
         Self::Date(value)
     }
 }
 
-impl<M: Tvf + Clone> From<NaiveDateTime> for TvfValue<'_, M> {
+impl<T: Tvf + Clone> From<NaiveDateTime> for TvfValue<'_, T> {
     fn from(value: NaiveDateTime) -> Self {
         Self::DateTime(value)
     }
 }
 
-impl<M: Tvf + Clone> From<M> for TvfValue<'_, M> {
-    fn from(value: M) -> Self {
+impl<T: Tvf + Clone> From<T> for TvfValue<'_, T> {
+    fn from(value: T) -> Self {
         Self::Buffer(Cow::Owned(value))
     }
 }
