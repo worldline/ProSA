7ac0b3327f9553fc22cd8ec3dda3d2cbe7c87677
reneca <reneca@users.noreply.github.com>
2026-03-25 08:10:04 +0000

    deploy: 7fda385f2bdbdd1cde7cc28ab11e2e78634ac180

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-82f6d18b.js',
+                path_to_root + 'searchindex-d1c2e4de.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
